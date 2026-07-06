dad234de04540a7b6431dc09a7074e08ec20120c
Aypara88 <Aypara88@users.noreply.github.com>
2026-07-06 11:09:55 -0700

    Implement outline: apply changes

---
+++ src/imports/Fv/index.tsx
@@ -0,0 +1,153 @@
+import svgPaths from "./svg-tx1p5gf62o";
+
+function Group1() {
+  return (
+    <div className="absolute left-0 size-[40px] top-0">
+      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 40 40">
+        <g id="Group 47">
+          <circle cx="20" cy="20" id="Ellipse 26" r="18.5" stroke="var(--stroke-0, #FA5B76)" strokeWidth="3" />
+          <line id="Line 66" stroke="var(--stroke-0, #FA5B76)" strokeWidth="3" x1="9.8549" x2="15.3293" y1="20.9603" y2="26.6537" />
+          <line id="Line 67" stroke="var(--stroke-0, #FA5B76)" strokeWidth="3" x1="30.1156" x2="14.9888" y1="10.935" y2="28.3551" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Group2() {
+  return (
+    <div className="absolute left-0 size-[40px] top-0">
+      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 40 40">
+        <g id="Group 47">
+          <circle cx="20" cy="20" id="Ellipse 26" r="18.5" stroke="var(--stroke-0, #FA5B76)" strokeWidth="3" />
+          <line id="Line 66" stroke="var(--stroke-0, #FA5B76)" strokeWidth="3" x1="9.8549" x2="15.3293" y1="20.9603" y2="26.6537" />
+          <line id="Line 67" stroke="var(--stroke-0, #FA5B76)" strokeWidth="3" x1="30.1156" x2="14.9888" y1="10.935" y2="28.3551" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Group3() {
+  return (
+    <div className="absolute left-0 size-[40px] top-0">
+      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 40 40">
+        <g id="Group 47">
+          <circle cx="20" cy="20" id="Ellipse 26" r="18.5" stroke="var(--stroke-0, #FA5B76)" strokeWidth="3" />
+          <line id="Line 66" stroke="var(--stroke-0, #FA5B76)" strokeWidth="3" x1="9.8549" x2="15.3293" y1="20.9603" y2="26.6537" />
+          <line id="Line 67" stroke="var(--stroke-0, #FA5B76)" strokeWidth="3" x1="30.1156" x2="14.9888" y1="10.935" y2="28.3551" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function SubCopy() {
+  return (
+    <div className="absolute contents left-0 top-0" data-name="sub_copy">
+      <div className="[word-break:break-word] absolute font-['Noto_Serif_JP:Bold',sans-serif] font-bold leading-[0] left-0 text-[0px] text-black text-shadow-[0px_0px_15px_white] top-0 tracking-[5.8px] whitespace-nowrap">
+        <p className="leading-[68px] mb-0 text-[36px]">マッチングアプリで</p>
+        <p className="leading-[68px] text-[54px]">出会った彼…</p>
+      </div>
+      <div className="absolute h-0 left-0 top-0 w-[617px]">
+        <div className="absolute inset-[-2px_-0.32%]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 621 4">
+            <path d="M2 2H619" id="Line 65" stroke="var(--stroke-0, #FA5B76)" strokeDasharray="8 8" strokeLinecap="round" strokeWidth="4" />
+          </svg>
+        </div>
+      </div>
+      <div className="absolute h-0 left-0 top-0 w-[617px]">
+        <div className="absolute inset-[-2px_-0.32%]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 621 4">
+            <path d="M2 2H619" id="Line 65" stroke="var(--stroke-0, #FA5B76)" strokeDasharray="8 8" strokeLinecap="round" strokeWidth="4" />
+          </svg>
+        </div>
+      </div>
+      <p className="[word-break:break-word] absolute font-['Noto_Serif_JP:Bold',sans-serif] font-bold leading-[1.5] left-0 text-[32px] text-white top-0 tracking-[3.2px] whitespace-nowrap">昼間や休日になかなか会えない…</p>
+      <p className="[word-break:break-word] absolute font-['Noto_Serif_JP:Bold',sans-serif] font-bold leading-[1.5] left-0 text-[32px] text-white top-0 tracking-[3.2px] whitespace-nowrap">結婚の話になると話題を変えられる</p>
+      <p className="[word-break:break-word] absolute font-['Noto_Serif_JP:Bold',sans-serif] font-bold leading-[1.5] left-0 text-[32px] text-white top-0 tracking-[3.2px] whitespace-nowrap">友人や家族を紹介してくれない</p>
+      <Group1 />
+      <Group2 />
+      <Group3 />
+    </div>
+  );
+}
+
+function Group() {
+  return (
+    <div className="absolute contents left-0 top-0">
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Noto_Serif_JP:Medium',sans-serif] font-medium leading-[0] left-0 text-[0px] text-black text-center text-shadow-[0px_0px_8px_white] top-0 tracking-[2.8px] w-[365px]">
+        <span className="font-['Noto_Serif_JP:Bold',sans-serif] font-bold leading-[32px] text-[36px]">たった</span>
+        <span className="font-['Noto_Serif_JP:Bold',sans-serif] font-bold leading-[32px] text-[#fa5b76] text-[48px]">1分</span>
+        <span className="font-['Noto_Serif_JP:Bold',sans-serif] font-bold leading-[32px] text-[36px]">で診断</span>
+      </p>
+      <div className="absolute flex h-[54.929px] items-center justify-center left-0 top-0 w-[31.713px]">
+        <div className="flex-none rotate-60 scale-y-98 skew-x-[-10.1deg]">
+          <div className="h-0 relative w-[63.426px]">
+            <div className="absolute inset-[-1.5px_0_-1.5px_-2.36%]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 64.9264 3">
+                <path d={svgPaths.p187be880} fill="var(--stroke-0, #FA5B76)" id="Line 6" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute flex h-[54.929px] items-center justify-center left-0 top-0 w-[31.713px]">
+        <div className="-scale-y-98 flex-none rotate-120 skew-x-[10.1deg]">
+          <div className="h-0 relative w-[63.426px]">
+            <div className="absolute inset-[-1.5px_0_-1.5px_-2.36%]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 64.9264 3">
+                <path d={svgPaths.p187be880} fill="var(--stroke-0, #FA5B76)" id="Line 7" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Group4() {
+  return (
+    <div className="absolute left-0 size-[40px] top-0">
+      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 40 40">
+        <g id="Group 50">
+          <circle cx="20" cy="20" id="Ellipse 26" r="18.5" stroke="var(--stroke-0, white)" strokeWidth="3" />
+          <line id="Line 66" stroke="var(--stroke-0, white)" strokeWidth="3" x1="9.8549" x2="15.3293" y1="20.9603" y2="26.6537" />
+          <line id="Line 67" stroke="var(--stroke-0, white)" strokeWidth="3" x1="30.1156" x2="14.9888" y1="10.935" y2="28.3551" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Cta() {
+  return (
+    <div className="-translate-x-1/2 absolute contents left-0 top-0" data-name="CTAボタン １">
+      <div className="absolute bg-gradient-to-b border border-solid border-white from-[#dd586e] h-[80px] left-0 rounded-[45px] to-[#e07488] to-[91.25%] top-0 w-[667px]" />
+      <Group />
+      <p className="[word-break:break-word] absolute font-['Noto_Sans_JP:Black',sans-serif] font-black leading-[20px] left-0 text-[32px] text-shadow-[0px_2px_2px_rgba(0,0,0,0.25)] text-white top-0 tracking-[6.4px] whitespace-nowrap">既婚者か診断する</p>
+      <Group4 />
+    </div>
+  );
+}
+
+export default function Fv() {
+  return (
+    <div className="contents relative size-full" data-name="FV">
+      <div className="[word-break:break-word] absolute font-['Noto_Serif_JP:Bold',sans-serif] font-bold leading-[0] left-0 text-[#374151] text-[0px] text-shadow-[0px_0px_25px_white] top-0 tracking-[5.6px] whitespace-nowrap">
+        <p className="font-['Noto_Serif_JP:SemiBold',sans-serif] font-semibold mb-0">
+          <span className="leading-[90px] text-[58px] text-black">もしかして</span>
+          <span className="leading-[90px] text-[#fa5b76] text-[80px]">彼</span>
+          <span className="leading-[90px] text-[58px] text-black">、</span>
+        </p>
+        <p>
+          <span className="font-['Noto_Serif_JP:Black',sans-serif] font-black leading-[1.2] text-[#fa5b76] text-[90px]">既婚者</span>
+          <span className="font-['Noto_Serif_JP:SemiBold',sans-serif] font-semibold leading-[90px] text-[70px] text-black">かもしれない…</span>
+        </p>
+      </div>
+      <SubCopy />
+      <Cta />
+    </div>
+  );
+}
